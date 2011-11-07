c0c0e3ecee7d9a6ab6a76bc7d68ac323a44b86b3
Phil Hambly <phil.hambly@gmail.com>
2011-11-07 22:10:56 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patman::Application.config.secret_token = '5d45c7aa9a96c66cad2a4743220f2e3d09182e5d1b345505e82aa801fbe69ee64e9b22f6d7d8d6f0271ad8cbeafddd2e4683f70442617ca3d2bfd7bfeda2e2da'
