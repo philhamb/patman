668863f33f8b3f8839d7fb6d8ced17825e6420bc
Phil Hambly <phil.hambly@gmail.com>
2011-11-08 14:24:55 +0700

    static pages created home admin and support

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Patman::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/admin"
+
+  get "pages/support"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
