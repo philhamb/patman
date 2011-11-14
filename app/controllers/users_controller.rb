d4dabdd219e3fccc601f836ef8ae1bf671d10864
Phil Hambly <phil.hambly@gmail.com>
2011-11-14 17:23:41 +0700

    Done with user edit/update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update] 
-  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy] 
+  before_filter :correct_user, :only => [ :edit, :update]
+  before_filter :admin_user,   :only => :destroy
 
   def new
     @title = "Sign up"
     @user = User.new
   end
-  
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end  
 
   def show
     @user = User.find(params[:id]) 
@@ -24,7 +29,6 @@ class UsersController < ApplicationController
       @user.password = ""
       @user.password_confirmation = ""
       render "new"
-
     end
   end
 
@@ -42,6 +46,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
   private
     
     def authenticate
@@ -52,5 +62,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end  
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111111075926) do
+ActiveRecord::Schema.define(:version => 20111114093157) do
 
   create_table "patients", :force => true do |t|
     t.string   "f_name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20111111075926) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
