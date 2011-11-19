dd7e7ce24da9f6fa73da38da5ecc76107aa0fd3e
Phil Hambly <phil.hambly@gmail.com>
2011-11-19 21:39:00 +0700

    more on new patientremoved formtastic and simple form

---
+++ spec/requests/patients_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Patients" do
+  describe "new_p" do
+    describe "failure" do
+      it "should not make a new user" do
+        lambda do
+          visit new_p_path
+          fill_in "First Name",     :with => ""
+          fill_in "Surname",        :with => ""
+          fill_in "Email Address",  :with => ""
+          click_button
+          response.should render_template('patients/new')
+          response.should have_selector("div#error_explanation")
+        end.should_not change(Patient , :count)
+      end
+    end
+ 
+    describe "success" do
+    
+      it "should make a new patient" do
+        lambda do
+          visit new_p_path
+          fill_in "First Name",     :with => "Example"
+          fill_in "Surname",        :with => "Patient"
+          fill_in "Email Address",  :with => "example@patient.com"
+          selects "1961"
+          selects "July"
+          selects "15"
+          chooses "patient_gender_male"
+          click_button
+          response.should have_selector("div.flash.success",
+                                        :content => "New patient")
+          response.should render_template('patients/show')
+        end.should change(Patient , :count).by(1)
+      end
+    end
+  end
+end
+
+    
+      
